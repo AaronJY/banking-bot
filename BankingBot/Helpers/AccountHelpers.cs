dbc425ea9af4c6c3ff7352513d45694b94ab0aa6
AaronJamesY <personal.ajy@gmail.com>
2017-02-10 18:51:03 +0000

    Moved helper function from CardHelpers to AccountHelpers

--- BankingBot/Helpers/AccountHelpers.cs
+++ BankingBot/Helpers/AccountHelpers.cs
@@ -1,8 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace BankingBot.Helpers
 {
-    public static class CardHelpers
+    public static class AccountHelpers
     {
         public static string[] SplitCardNumber(string cardNumber)
         {
