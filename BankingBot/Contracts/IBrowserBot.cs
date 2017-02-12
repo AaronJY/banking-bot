24554dde870ad7dc20eb3d5328323bc1118a5696
AaronJamesY <personal.ajy@gmail.com>
2017-02-12 23:00:48 +0000

    Client and dependencies are now properly disposed of

--- BankingBot/Contracts/IBrowserBot.cs
+++ BankingBot/Contracts/IBrowserBot.cs
@@ -1,8 +1,9 @@
 ﻿using OpenQA.Selenium;
+using System;
 
 namespace BankingBot.Contracts
 {
-    public interface IBrowserBot
+    public interface IBrowserBot : IDisposable
     {
         IWebDriver WebDriver { get; }
     }
